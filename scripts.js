176133068edd339a4e1af256b746f39195a4ddc2
Danny C <35072063+dchen705@users.noreply.github.com>
2023-02-25 16:45:26 -0500

    Apply grid layout to containers

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const Gameboard = (function() {
+    const spaces = ["","","","","","","","",""];
+    return {spaces};
+})();
+
+const Player = (name) => {
+    return {name, wins, losses, ties}
+}
+
+const displayController = (function() {
+    
+})();
