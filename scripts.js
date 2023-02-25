bc7a19fd43285eebe0c0b3218a05f1bd8ee139d5
Danny C <35072063+dchen705@users.noreply.github.com>
2023-02-25 18:15:12 -0500

    Make marking space function and start button logic and apply to gameboard

--- scripts.js
+++ scripts.js
@@ -1,6 +1,25 @@
-const Gameboard = (function() {
-    const spaces = ["","","","","","","","",""];
-    return {spaces};
+const gameboard = document.querySelector(".tictactoe-board");
+const spaces = document.querySelectorAll(".tictactoe-board>div");
+let turn_counter = 1;
+
+
+function mark_space(space_num) {
+    console.log(space_num);
+    let space_text = space_num.querySelector('p');
+    if (turn_counter % 2 == 1) {
+        space_text.textContent="X";
+    }
+    else if (turn_counter % 2 == 0) {
+        space_text.textContent="O";
+    }
+    console.log(turn_counter);
+    turn_counter += 1;
+    
+}
+
+const gameboardModule = (function() {
+    const space_values = ["","","","","","","","",""];
+    return {space_values};
 })();
 
 const Player = (name) => {
@@ -8,5 +27,18 @@ const Player = (name) => {
 }
 
 const displayController = (function() {
-    
+
+})();
+
+const gameController = (function() {
+    const start_btn = document.querySelector(".start_button");
+    start_btn.addEventListener("click", () => {
+        gameboard.style.backgroundColor = "white";
+        turn_counter = 1;
+        spaces.forEach(function(space) {
+            space.addEventListener("click", function(event) {
+                mark_space(space);
+            });   
+        });
+    });
 })();
